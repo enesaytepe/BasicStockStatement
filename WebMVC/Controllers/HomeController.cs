b93476b8121ebf2ff2493232e780f2a5199ec5f5
Enes Aytepe <enes_aytepe@hotmail.com>
2023-10-10 11:24:03 +0300

    Added new controller and page for paged list

--- WebMVC/Controllers/HomeController.cs
+++ WebMVC/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using Data.RequestModels;
+﻿using Data.Models;
+using Data.RequestModels;
 using DataAccess;
 using DataAccess.Models;
 using DataAccess.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Diagnostics;
 using WebMVC.Models;
 
@@ -30,7 +32,7 @@ namespace WebMVC.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-
+        [HttpGet]
         public async Task<IActionResult> StockMovementList()
         {
             // Model geçerli değilse hata mesajlarıyla birlikte formu tekrar göster
@@ -40,26 +42,107 @@ namespace WebMVC.Controllers
         [HttpPost]
         public async Task<IActionResult> StockMovementList(StockMovementFilterModel model)
         {
-            if (ModelState.IsValid)
+            //Repository tanımlaması yapılır.
+            StockRepository stockRepository = new StockRepository(_dbContext);
+
+            //Veriler uygun hale getirilir.
+            string stockCode = model.StockCode.Trim();
+            int startDate = Convert.ToInt32((model.StartDate).ToOADate());
+            int endDate = Convert.ToInt32((model.EndDate).ToOADate());
+
+            //Sonuç repository'den alınır.
+            List<StockMovement> stockMovementList = await stockRepository.GetStockMovements(stockCode, startDate, endDate);
+
+            if (stockMovementList != null && stockMovementList.Count > 0)
             {
-                StockRepository stockRepository = new StockRepository(_dbContext);
+                //Eğer data varsa stok hesaplaması yapılır.
+                decimal stock = 0;
 
-                //string stockCode = "10081 SİEMENS";
-                //int startDate = 42736;
-                //int endDate = 42775;
+                foreach (StockMovement item in stockMovementList)
+                {
+                    //TODO string karşılaştırması değiştirilmesi gerekli.
+                    if (item.IslemTur == "Giriş")
+                    {
+                        stock = stock + item.GirisMiktar;
+                    }
+                    else if (item.IslemTur == "Çıkış")
+                    {
+                        stock = stock - item.CikisMiktar;
+                    }
+                    else
+                    {
+                        //do nothing
+                    }
 
-                string stockCode = model.StockCode;
+                    item.Stok = stock;
+                }
+
+                return View(stockMovementList);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PagedStockMovementList(PagedStockMovementFilterModel model)
+        {
+            if (!String.IsNullOrEmpty(model.StockCode))
+            {
+                //Veriler uygun hale getirilir.
+                string stockCode = model.StockCode.Trim();
                 int startDate = Convert.ToInt32((model.StartDate).ToOADate());
                 int endDate = Convert.ToInt32((model.EndDate).ToOADate());
+               int pageNumber = model.PageNumber < 1 ? 1 : model.PageNumber;
 
-                var data = await stockRepository.GetStockMovements(stockCode, startDate, endDate);
+                //Default page size tanımlaması
+                int pageSize = 5;
 
-                // Filtrelenmiş verileri view'e gönder
-                return View(data);
+                //Repository tanımlaması yapılır.
+                StockRepository stockRepository = new StockRepository(_dbContext);
+
+                //Sonuç repository'den alınır.
+                PagedDataModel<StockMovement> stockMovementList = await stockRepository.GetPagedStockMovements(stockCode, startDate, endDate, pageNumber, pageSize);
+
+                //Toplam sayfa sayısı, sayfalama elementlerinin oluşturulabilmesi için gönderilir.
+                ViewData["TotalPages"] = stockMovementList.TotalPage;
+
+                if (stockMovementList != null && stockMovementList.DataList != null && stockMovementList.DataList.Count > 0)
+                {
+                    //Eğer data varsa stok hesaplaması yapılır.
+                    decimal stock = 0;
+
+                    //TODO Stok hesaplaması, sayfalamalı stok hareketleri sayfasında nasıl olmalı?
+
+                    //foreach (StockMovement item in stockMovementList.DataList)
+                    //{
+                    //    //TODO string karşılaştırması değiştirilmesi gerekli.
+                    //    if (item.IslemTur == "Giriş")
+                    //    {
+                    //        stock = stock + item.GirisMiktar;
+                    //    }
+                    //    else if (item.IslemTur == "Çıkış")
+                    //    {
+                    //        stock = stock - item.CikisMiktar;
+                    //    }
+                    //    else
+                    //    {
+                    //        //do nothing
+                    //    }
+
+                    //    item.Stok = stock;
+                    //}
+
+                    PagedStockMovementListViewModel pagedStockMovementListViewModel = new PagedStockMovementListViewModel();
+                    pagedStockMovementListViewModel.PagedData = stockMovementList;
+                    pagedStockMovementListViewModel.Filter = model;
+
+                    return View(pagedStockMovementListViewModel);
+                }
             }
 
-            // Model geçerli değilse hata mesajlarıyla birlikte formu tekrar göster
-            return View(model);
+            return View();
         }
     }
 }
---
+++ WebMVC/Models/PagedStockMovementListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Data.Models;
+using Data.RequestModels;
+
+namespace WebMVC.Models
+{
+    public class PagedStockMovementListViewModel
+    {
+        public PagedDataModel<StockMovement> PagedData { get; set; }
+        public PagedStockMovementFilterModel Filter { get; set; }
+    }
+}
