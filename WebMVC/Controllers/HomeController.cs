26157aac6fafe14a7af0dd78af7773d8d1e6da62
Enes Aytepe <enes_aytepe@hotmail.com>
2023-10-10 09:32:44 +0300

    Created new controller and new page for stock movement list

--- WebMVC/Controllers/HomeController.cs
+++ WebMVC/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Data.RequestModels;
+using DataAccess;
+using DataAccess.Models;
+using DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebMVC.Models;
 
@@ -7,18 +11,15 @@ namespace WebMVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly TestContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, TestContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
@@ -27,6 +28,38 @@ namespace WebMVC.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public async Task<IActionResult> StockMovementList()
+        {
+            // Model geçerli değilse hata mesajlarıyla birlikte formu tekrar göster
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> StockMovementList(StockMovementFilterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                StockRepository stockRepository = new StockRepository(_dbContext);
+
+                //string stockCode = "10081 SİEMENS";
+                //int startDate = 42736;
+                //int endDate = 42775;
+
+                string stockCode = model.StockCode;
+                int startDate = Convert.ToInt32((model.StartDate).ToOADate());
+                int endDate = Convert.ToInt32((model.EndDate).ToOADate());
+
+                var data = await stockRepository.GetStockMovements(stockCode, startDate, endDate);
+
+                // Filtrelenmiş verileri view'e gönder
+                return View(data);
+            }
+
+            // Model geçerli değilse hata mesajlarıyla birlikte formu tekrar göster
+            return View(model);
         }
     }
 }
