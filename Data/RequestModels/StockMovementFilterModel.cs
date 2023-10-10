fe022351b9ac7fe64fb4d47d2236cee086857446
Enes Aytepe <enes_aytepe@hotmail.com>
2023-10-10 09:30:00 +0300

    Created filter model for stock movement filtering

---
+++ Data/RequestModels/StockMovementFilterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Data.RequestModels
+{
+    public class StockMovementFilterModel
+    {
+        public string StockCode { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
