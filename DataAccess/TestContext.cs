70d938e0659ad4db134909053bd3119a152abb3b
Enes Aytepe <enes_aytepe@hotmail.com>
2023-10-10 09:32:01 +0300

    Added generated ef scaffold codes

--- DataAccess/TestContext.cs
+++ DataAccess/TestContext.cs
@@ -6,31 +6,77 @@ namespace DataAccess
 {
     public partial class TestContext : DbContext
     {
-        public TestContext(DbContextOptions<TestContext> options)
-            : base(options)
+        public TestContext(DbContextOptions<TestContext> options) : base(options)
         {
         }
 
         //Tables
-        public DbSet<Sti> Stis { get; set; }
-        public DbSet<Stk> Stks { get; set; }
+        public virtual DbSet<Sti> Stis { get; set; } = null!;
+        public virtual DbSet<Stk> Stks { get; set; } = null!;
 
         //Stored Procedures
         public DbSet<StockMovement> StockMovements { get; set; }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    // Prosedürü bağlama
-        //    modelBuilder.Entity<StockMovement>().HasNoKey();
-        //    modelBuilder.Query<StockMovement>().ToView("GetStockMovements");
-        //}
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
+            modelBuilder.UseCollation("Turkish_CI_AS");
+
+            modelBuilder.Entity<Sti>(entity =>
             {
-                optionsBuilder.UseSqlServer();
-            }
+                entity.HasKey(e => new { e.EvrakNo, e.Tarih, e.IslemTur })
+                    .HasName("pkSTI");
+
+                entity.ToTable("STI");
+
+                entity.Property(e => e.EvrakNo)
+                    .HasMaxLength(16)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Birim)
+                    .HasMaxLength(4)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Fiyat).HasColumnType("numeric(25, 6)");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.MalKodu)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Miktar).HasColumnType("numeric(25, 6)");
+
+                entity.Property(e => e.Tutar).HasColumnType("numeric(25, 6)");
+            });
+
+            modelBuilder.Entity<Stk>(entity =>
+            {
+                entity.HasKey(e => e.MalKodu)
+                    .HasName("pkSTK");
+
+                entity.ToTable("STK");
+
+                entity.Property(e => e.MalKodu)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.MalAdi)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            // Prosedürü bağlama
+            modelBuilder.Entity<StockMovement>().HasKey(s => s.SiraNo);
+
+            OnModelCreatingPartial(modelBuilder);
         }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
