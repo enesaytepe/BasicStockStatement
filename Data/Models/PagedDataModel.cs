254e9507a7167efe273db1f8787ca7380ec896ea
Enes Aytepe <enes_aytepe@hotmail.com>
2023-10-10 11:22:10 +0300

    Created model for paged data

---
+++ Data/Models/PagedDataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.Models
+{
+    public class PagedDataModel<T>
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPage { get; set; }
+        public List<T> DataList { get; set; }
+    }
+}
