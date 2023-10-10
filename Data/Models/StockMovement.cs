837ce8ea0936181ef8404a04df08980de81d5248
Enes Aytepe <enes_aytepe@hotmail.com>
2023-10-10 06:58:25 +0300

    Added model for GetStockMovement stored procedure returning data

---
+++ Data/Models/StockMovement.cs
@@ -0,0 +1,16 @@
+﻿namespace Data.Models
+{
+    /// <summary>
+    /// GetStockMovements(Stored Procedure) dönüş bilgilerini almak için oluşturuldu.
+    /// </summary>
+    public class StockMovement
+    {
+        public int SiraNo { get; set; }
+        public string IslemTur { get; set; }
+        public string EvrakNo { get; set; }
+        public string Tarih { get; set; }
+        public decimal GirisMiktar { get; set; }
+        public decimal CikisMiktar { get; set; }
+        public decimal Stok { get; set; }
+    }
+}
