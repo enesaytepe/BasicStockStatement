d3810c4a3090d1a39dcae9c4eb660bd19cecd96a
Enes Aytepe <enes_aytepe@hotmail.com>
2023-10-10 16:12:30 +0300

    Created new project for DevExpress

---
+++ WebDxMVC/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using Data.Models;
+using Data.RequestModels;
+using DataAccess;
+using DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebDxMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly TestContext _dbContext;
+
+        public HomeController(TestContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> StockMovementList()
+        {
+            // Model geçerli değilse hata mesajlarıyla birlikte formu tekrar göster
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> StockMovementList(StockMovementFilterModel model)
+        {
+            //Repository tanımlaması yapılır.
+            StockRepository stockRepository = new StockRepository(_dbContext);
+
+            //Veriler uygun hale getirilir.
+            string stockCode = model.StockCode.Trim();
+            int startDate = Convert.ToInt32((model.StartDate).ToOADate());
+            int endDate = Convert.ToInt32((model.EndDate).ToOADate());
+
+            //Sonuç repository'den alınır.
+            List<StockMovement> stockMovementList = await stockRepository.GetStockMovements(stockCode, startDate, endDate);
+
+            if (stockMovementList != null && stockMovementList.Count > 0)
+            {
+                //Eğer data varsa stok hesaplaması yapılır.
+                decimal stock = 0;
+
+                foreach (StockMovement item in stockMovementList)
+                {
+                    //TODO string karşılaştırması değiştirilmesi gerekli.
+                    if (item.IslemTur == "Giriş")
+                    {
+                        stock = stock + item.GirisMiktar;
+                    }
+                    else if (item.IslemTur == "Çıkış")
+                    {
+                        stock = stock - item.CikisMiktar;
+                    }
+                    else
+                    {
+                        //do nothing
+                    }
+
+                    item.Stok = stock;
+                }
+
+                return View(stockMovementList);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebDxMVC/Program.cs
@@ -0,0 +1,32 @@
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services
+    .AddControllersWithViews()
+    .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+builder.Services.AddDbContext<TestContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
