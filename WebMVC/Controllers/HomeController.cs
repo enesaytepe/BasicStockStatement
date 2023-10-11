d01d34cc0e504f006722f9a945e20e930712a638
Enes Aytepe <enes_aytepe@hotmail.com>
2023-10-11 13:21:24 +0300

    Redesigned controller

--- WebMVC/Controllers/HomeController.cs
+++ WebMVC/Controllers/HomeController.cs
@@ -1,7 +1,8 @@
-﻿using Data.Models;
+﻿using Common;
+using Data.Models;
 using Data.RequestModels;
-using DataAccess;
-using DataAccess.Repositories;
+using DataAccess.Interfaces;
+using DataAccess.Repositories.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebMVC.Models;
@@ -11,12 +12,14 @@ namespace WebMVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly TestContext _dbContext;
+        private readonly IConnectionStringProvider _connectionStringProvider;
+        private readonly IStockRepository _stockRepository;
 
-        public HomeController(ILogger<HomeController> logger, TestContext dbContext)
+        public HomeController(ILogger<HomeController> logger, IConnectionStringProvider connectionStringProvider, IStockRepository stockRepository)
         {
             _logger = logger;
-            _dbContext = dbContext;
+            _connectionStringProvider = connectionStringProvider;
+            _stockRepository = stockRepository;
         }
 
         public IActionResult Index()
@@ -40,16 +43,13 @@ namespace WebMVC.Controllers
         [HttpPost]
         public async Task<IActionResult> StockMovementList(StockMovementFilterModel model)
         {
-            //Repository tanımlaması yapılır.
-            StockRepository stockRepository = new StockRepository(_dbContext);
-
             //Veriler uygun hale getirilir.
             string stockCode = model.StockCode.Trim();
             int startDate = Convert.ToInt32((model.StartDate).ToOADate());
             int endDate = Convert.ToInt32((model.EndDate).ToOADate());
 
             //Sonuç repository'den alınır.
-            List<StockMovement> stockMovementList = await stockRepository.GetStockMovements(stockCode, startDate, endDate);
+            List<StockMovement> stockMovementList = await _stockRepository.GetStockMovements(stockCode, startDate, endDate);
 
             if (stockMovementList != null && stockMovementList.Count > 0)
             {
@@ -92,16 +92,13 @@ namespace WebMVC.Controllers
                 string stockCode = model.StockCode.Trim();
                 int startDate = Convert.ToInt32((model.StartDate).ToOADate());
                 int endDate = Convert.ToInt32((model.EndDate).ToOADate());
-               int pageNumber = model.PageNumber < 1 ? 1 : model.PageNumber;
+                int pageNumber = model.PageNumber < 1 ? 1 : model.PageNumber;
 
                 //Default page size tanımlaması
                 int pageSize = 5;
 
-                //Repository tanımlaması yapılır.
-                StockRepository stockRepository = new StockRepository(_dbContext);
-
                 //Sonuç repository'den alınır.
-                PagedDataModel<StockMovement> stockMovementList = await stockRepository.GetPagedStockMovements(stockCode, startDate, endDate, pageNumber, pageSize);
+                PagedDataModel<StockMovement> stockMovementList = await _stockRepository.GetPagedStockMovements(stockCode, startDate, endDate, pageNumber, pageSize);
 
                 //Toplam sayfa sayısı, sayfalama elementlerinin oluşturulabilmesi için gönderilir.
                 ViewData["TotalPages"] = stockMovementList.TotalPage;
