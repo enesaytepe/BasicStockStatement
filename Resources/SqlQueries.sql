0cd17633b72806d7c612ebe1fe172414b3c7c7d0
Enes Aytepe <enes_aytepe@hotmail.com>
2023-10-11 14:38:39 +0300

    Added sql query files

---
+++ Resources/SqlQueries.sql
@@ -0,0 +1,34 @@
+﻿SELECT TOP (1000) [ID]
+      ,[MalKodu]
+      ,[MalAdi]
+  FROM [Test].[dbo].[STK]
+
+
+  SELECT TOP (1000) [ID]
+      ,[IslemTur]
+      ,[EvrakNo]
+      ,[Tarih]
+      ,[MalKodu]
+      ,[Miktar]
+      ,[Fiyat]
+      ,[Tutar]
+      ,[Birim]
+  FROM [Test].[dbo].[STI]
+
+------------------------------------------------------------------------
+
+EXEC GetStockMovements @Malkodu = '10081 SİEMENS', @BaslangicTarihi = '42736', @BitisTarihi = '42775';
+
+------------------------------------------------------------------------
+
+DECLARE @TotalPages INT; 
+
+EXEC GetPagedStockMovements 
+    @MalKodu = '10081 SİEMENS', 
+    @BaslangicTarihi = 42736, 
+    @BitisTarihi = 42775, 
+    @PageNumber = 1, 
+    @PageSize = 38,
+    @TotalPages = @TotalPages OUTPUT; 
+
+PRINT 'Total Pages: ' + CAST(@TotalPages AS NVARCHAR(10));
